7fe941416167fe306d557a2943b6c7bb4db484b3
mediamanipulation <manipulation2000@hotmail.com>
2019-11-08 01:01:25 -0600

    ui names button styling

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,11 +1,35 @@
 import React from 'react';
-import {ListGroup, ListGroupItem} from 'reactstrap';
+import {ListGroup, ListGroupItem, Button} from 'reactstrap';
+import { SSL_OP_NO_TLSv1_1 } from 'constants';
 
 const UsersList = ({users}) =>{
     return(
         <ListGroup>
-            {users.map((user) => {
-                return <ListGroupItem key={user.id}>{user.firstName}</ListGroupItem>
+            {users.sort((a,b)=>{
+                if(a.firstName > b.firstName){
+                    return 1;
+                }else if(a.firstName < b.firstName){
+                    return -1;
+                }else if(a.lastName > b.lastName){
+                    return 1;
+                }else if(a.lastName < b.lastName){
+                    return -1;
+                }else{
+                    return 0;
+                }
+            }).map((user) => {
+                return (
+                    <ListGroupItem key={user.id}>
+                    <section style={{display: 'flex'}}>
+                        <div  style={{flexGrow: 1}}>
+                            {user.firstName} {user.lastName}
+                        </div>
+                        <Button outline color='danger' >
+
+                        </Button>
+                    </section>
+                    </ListGroupItem>
+                ); 
             })}
         </ListGroup>
     ) 
